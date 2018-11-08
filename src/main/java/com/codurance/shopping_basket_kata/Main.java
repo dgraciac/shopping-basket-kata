dd6dc02d070e94573d67477ea9f88153ca0b8eca
David Gracia <david.gracia.celemendi@gmail.com>
2018-11-08 10:29:04 +0100

    project configuration

---
+++ src/main/java/com/codurance/shopping_basket_kata/Main.java
@@ -0,0 +1,9 @@
+package com.codurance.shopping_basket_kata;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String... args) throws IOException {
+        new Console().read();
+    }
+}
